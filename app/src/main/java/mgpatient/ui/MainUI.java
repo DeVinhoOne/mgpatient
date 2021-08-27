4c4d2df02ed71f2213cb94f140a6db753bc0eb3f
dawid <caushered@gmail.com>
2021-08-27 09:39:42 +0200

    create separate ui methods for addPatient and addDoctor

--- app/src/main/java/mgpatient/ui/MainUI.java
+++ app/src/main/java/mgpatient/ui/MainUI.java
@@ -23,43 +23,51 @@ public class MainUI {
                 System.out.println("\nExiting MGPatient..");
                 return;
             } else if (option == 1) {
-                System.out.println("\nFields with \"*\" are required");
-                System.out.print("\n*Name: ");
-                String name = this.scanner.next();
-                System.out.print("*Surname: ");
-                String surname = this.scanner.next();
-                System.out.print("*Phone number: ");
-                String phoneNumber = this.scanner.next();
-                //Validate input
-                if (Validator.validateStringNotNullInput(new String[] {name, surname, phoneNumber})) {
-                    this.MGPatient.createPatient(name, surname, phoneNumber);
-                    System.out.println("\nNew patient has been added!");
-                } else {
-                    System.out.println("\nInvalid input!");
-                    System.out.println("None of the fields cannot be empty.");
-                }
+                addPatientUI();
             } else if (option == 2) {
-                System.out.println("\nFields with \"*\" are required");
-                System.out.print("\n*Name: ");
-                String name = this.scanner.next();
-                System.out.print("*Surname: ");
-                String surname = this.scanner.next();
-                System.out.print("*Phone number: ");
-                String phoneNumber = this.scanner.next();
-                System.out.print("Specialization: ");
-                String specialization = this.scanner.next();
-                System.out.print("E-mail: ");
-                String email = this.scanner.next();
-                if (Validator.validateStringNotNullInput(new String[] {name, surname, phoneNumber})) {
-                    this.MGPatient.createDoctor(name, surname, phoneNumber, specialization,email);
-                    System.out.println("\nNew doctor has been added.");
-                } else {
-                    System.out.println("\nInvalid input!");
-                    System.out.println("Make sure that required fields (*) are not empty.");
-                }
+                addDoctorUI();
             } else {
                 System.out.println("\nThere's no such option: " + option);
             }
+        }
+    }
+
+    private void addPatientUI() {
+        System.out.println("\nFields with \"*\" are required");
+        System.out.print("\n*Name: ");
+        String name = this.scanner.next();
+        System.out.print("*Surname: ");
+        String surname = this.scanner.next();
+        System.out.print("*Phone number: ");
+        String phoneNumber = this.scanner.next();
+        //Validate input
+        if (Validator.validateStringNotNullInput(new String[] {name, surname, phoneNumber})) {
+            this.MGPatient.createPatient(name, surname, phoneNumber);
+            System.out.println("\nNew patient has been added!");
+        } else {
+            System.out.println("\nInvalid input!");
+            System.out.println("None of the fields cannot be empty.");
+        }
+    }
+
+    private void addDoctorUI() {
+        System.out.println("\nFields with \"*\" are required");
+        System.out.print("\n*Name: ");
+        String name = this.scanner.next();
+        System.out.print("*Surname: ");
+        String surname = this.scanner.next();
+        System.out.print("*Phone number: ");
+        String phoneNumber = this.scanner.next();
+        System.out.print("Specialization: ");
+        String specialization = this.scanner.next();
+        System.out.print("E-mail: ");
+        String email = this.scanner.next();
+        if (Validator.validateStringNotNullInput(new String[] {name, surname, phoneNumber})) {
+            this.MGPatient.createDoctor(name, surname, phoneNumber, specialization,email);
+            System.out.println("\nNew doctor has been added.");
+        } else {
+            System.out.println("\nInvalid input!");
+            System.out.println("Make sure that required fields (*) are not empty.");
         }
     }
 }
